7e24a45a52158d7c190ab05792d07cef65fd8d01
Matthew Temple <matt@newcove.co.uk>
2017-05-29 00:54:02 +0800

    A static demo of what the page should look like

--- application/views/inbox.php
+++ application/views/inbox.php
@@ -2,4 +2,34 @@
     <div class="page-title">
         <h2>Inbox</h2>
     </div>
+    <div class="table-responsive">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Header</th>
+            <th>Header</th>
+            <th>Header</th>
+            <th>Header</th>
+          </tr>
+        </thead>
+        <tbody>
+            <?php
+                // output looped table to show UI sample
+                $limit = 25;
+                $count = 1;
+                while($limit > $count) :
+                    echo '<tr>
+                      <td>1,001</td>
+                      <td>Lorem</td>
+                      <td>ipsum</td>
+                      <td>dolor</td>
+                      <td>sit</td>
+                    </tr>';
+                    $count++;
+                endwhile;
+            ?>
+        </tbody>
+      </table>
+  </div>
 </div>
