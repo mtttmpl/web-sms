3c94065a90a91efb388262f8c9a81f985e824240
Matthew Temple <matt@newcove.co.uk>
2017-05-29 00:02:52 +0800

    Load basic views for inbox and sent

--- application/controllers/Inbox.php
+++ application/controllers/Inbox.php
@@ -1,8 +1,14 @@
 <?php
     class Inbox extends CI_Controller {
-        public function view($page = 1) {
+        public function index() {
             $this->load->view('templates/header');
+            $this->load->view('inbox');
+            $this->load->view('templates/footer');
+        }
 
+        public function sent() {
+            $this->load->view('templates/header');
+            $this->load->view('sent');
             $this->load->view('templates/footer');
         }
     }
