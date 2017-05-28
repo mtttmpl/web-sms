8982520db31821c1ace0a32ada7e2b2d842607b1
Matthew Temple <matt@newcove.co.uk>
2017-05-29 01:10:40 +0800

    Add nav element for mobile

--- application/views/templates/sidenav.php
+++ application/views/templates/sidenav.php
@@ -8,3 +8,11 @@
         <li <?php if($selected == 'contacts') { echo ' class="active"'; } ?>><a href="#">Contacts</a></li>
     </ul>
 </div>
+<div class="visible-xs-block col-xs-12 mobile-nav-container">
+    <ul class="nav nav-pills">
+        <li <?php if($selected == 'inbox') { echo ' class="active"'; } ?>><a href="<?php echo base_url(); ?>folders/inbox">Inbox</a></li>
+        <li <?php if($selected == 'sent') { echo ' class="active"'; } ?>><a href="<?php echo base_url(); ?>folders/sent">Sent</a></li>
+        <li <?php if($selected == 'archive') { echo ' class="active"'; } ?>><a href="#">Archive</a></li>
+        <li <?php if($selected == 'contacts') { echo ' class="active"'; } ?>><a href="#">Contacts</a></li>
+    </ul>
+</div>
