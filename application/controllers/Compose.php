3f630ae860593602c92a092ed72dfd81ba192d49
Matthew Temple <matt@newcove.co.uk>
2017-05-31 20:12:18 +0800

    Make fom submit and validate

--- application/controllers/Compose.php
+++ application/controllers/Compose.php
@@ -5,10 +5,19 @@
         }
 
         public function message() {
-            $sidenav['selected'] = 'none';
             $this->load->view('templates/header');
-            $this->load->view('templates/sidenav', $sidenav);
-            $this->load->view('compose');
+
+            $this->load->library('form_validation');
+            $this->form_validation->set_rules('to', 'To', 'required|min_length[6]|numeric');
+            $this->form_validation->set_rules('message', 'Message', 'required');
+            
+            if ($this->form_validation->run() == FALSE) :
+                $sidenav['selected'] = 'none';
+                $this->load->view('templates/sidenav', $sidenav);
+                $this->load->view('compose');
+            else :
+                redirect('folders/inbox');
+            endif;
             $this->load->view('templates/footer');
         }
     }
--- application/views/compose.php
+++ application/views/compose.php
@@ -10,18 +10,20 @@
     </div>
     <?php
         $form_attributes = array('class' => 'form-horizontal');
-        echo form_open('compose/send', $form_attributes);
+        echo form_open('compose/message', $form_attributes);
+
+        validation_errors('<div class="alert alert-danger" role="alert">', '</div>');
     ?>
         <div class="form-group">
             <label for="inputEmail3" class="col-sm-2 control-label">To</label>
             <div class="col-sm-10">
-            <input type="text" class="form-control" id="inputEmail3" placeholder="Number" name="to">
+            <input type="text" class="form-control" id="inputEmail3" placeholder="Number" name="to" value="<?php set_value('to'); ?>">
             </div>
         </div>
         <div class="form-group">
             <label for="inputMessage" class="col-sm-2 control-label">Message</label>
             <div class="col-sm-10">
-            <textarea class="form-control" id="inputMessage" placeholder="Message" name="message"></textarea>
+            <textarea class="form-control" id="inputMessage" placeholder="Message" name="message"><?php set_value('message'); ?</textarea>
             </div>
         </div>
         <div class="form-group">
