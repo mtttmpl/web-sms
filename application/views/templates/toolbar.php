94467da2866c7b8ecb9e43aee302590cb0b536a2
Matthew Temple <matt@newcove.co.uk>
2017-05-31 18:26:05 +0800

    Change compose link

--- application/views/templates/toolbar.php
+++ application/views/templates/toolbar.php
@@ -3,7 +3,7 @@
         if($page == 'compose') :
              echo '<a class='btn btn-default' href="javascript:window.history.back();"><i class="glyphicon glyphicon-remove"></i></a>';
         else :
-            echo '<a class='btn btn-default' href="'.base_url().'compose/new"><i class="glyphicon glyphicon-edit"></i></a>';
+            echo '<a class='btn btn-default' href="'.base_url().'compose/message"><i class="glyphicon glyphicon-edit"></i></a>';
         endif;
     ?>
 </div>
