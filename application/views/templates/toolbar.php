d57b35007622e349a792084037c24db62aa77e6f
Matthew Temple <matt@newcove.co.uk>
2017-05-31 18:29:53 +0800

    Fix error if page var is not passed

--- application/views/templates/toolbar.php
+++ application/views/templates/toolbar.php
@@ -1,6 +1,6 @@
 <div class="btn-group">
     <?php
-        if($page == 'compose') :
+        if(isset($page) && $page == 'compose') :
              echo '<a class="btn btn-default" href="javascript:window.history.back();"><i class="glyphicon glyphicon-remove"></i></a>';
         else :
             echo '<a class="btn btn-default" href="'.base_url().'compose/message"><i class="glyphicon glyphicon-edit"></i></a>';
