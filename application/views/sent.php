d38b94cda2c09154745923422cbef109c9a728bf
Matthew Temple <matt@newcove.co.uk>
2017-05-31 17:40:55 +0800

    Add toolbar to sent view

--- application/views/sent.php
+++ application/views/sent.php
@@ -1,6 +1,6 @@
 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
     <div class="page-title">
-        <h2>Sent</h2>
+        <h2>Sent<div class="toolbar pull-right"><?php $this->view('templates/toolbar'); ?></div></h2>
     </div>
     <div class="table-responsive">
       <table class="table table-striped">
