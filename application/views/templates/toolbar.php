10d75895d1247f44e1a5049c97c1b74c7d42eb1e
Matthew Temple <matt@newcove.co.uk>
2017-05-31 18:27:28 +0800

    fix php errors

--- application/views/templates/toolbar.php
+++ application/views/templates/toolbar.php
@@ -1,9 +1,9 @@
 <div class="btn-group">
     <?php
         if($page == 'compose') :
-             echo '<a class='btn btn-default' href="javascript:window.history.back();"><i class="glyphicon glyphicon-remove"></i></a>';
+             echo '<a class="btn btn-default" href="javascript:window.history.back();"><i class="glyphicon glyphicon-remove"></i></a>';
         else :
-            echo '<a class='btn btn-default' href="'.base_url().'compose/message"><i class="glyphicon glyphicon-edit"></i></a>';
+            echo '<a class="btn btn-default" href="'.base_url().'compose/message"><i class="glyphicon glyphicon-edit"></i></a>';
         endif;
     ?>
 </div>
