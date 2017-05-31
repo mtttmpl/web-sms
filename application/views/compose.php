ab479863100e8a92676f0e6690b91caf80fd0e30
Matthew Temple <matt@newcove.co.uk>
2017-05-31 18:22:59 +0800

    Compose View

---
+++ application/views/compose.php
@@ -0,0 +1,11 @@
+<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+    <div class="page-title">
+        <h2>Compose Message</h2>
+        <div class="toolbar pull-right">
+            <?php
+                $toolbar_data['page'] = 'compose'
+            ?>
+          <?php $this->view('templates/toolbar', $toolbar_data); ?>
+        </div>
+    </div>
+</div>
